20aedd73cf50593378c1d326e703d733e45fab61
Themerxitix <albormetaliaj@outlook.com>
2025-12-17 16:17:45 +0100

    feat: setup Spring Boot project met PostgreSQL database configuratie

---
+++ src/test/java/nl/demaker/demaker/DemakerApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.demaker.demaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
