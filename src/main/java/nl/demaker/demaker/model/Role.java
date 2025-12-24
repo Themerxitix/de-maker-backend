3292f5549bb966f359320d62a4ed5d3abcc0024d
Themerxitix <albormetaliaj@outlook.com>
2025-12-24 03:29:13 +0100

    feat: add model and repository

---
+++ src/main/java/nl/demaker/demaker/model/Role.java
@@ -0,0 +1,27 @@
+package nl.demaker.demaker.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
+    private Set<User> users = new HashSet<>();
+}
