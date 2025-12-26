81ce585e949082305eacd1853a27d37369410120
Themerxitix <albormetaliaj@outlook.com>
2025-12-26 16:07:34 +0100

    feat: add custom exception classes

--- src/main/java/nl/demaker/demaker/exception/ResourceNotFoundException.java
+++ src/main/java/nl/demaker/demaker/exception/ResourceNotFoundException.java
@@ -2,6 +2,10 @@ package nl.demaker.demaker.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
 
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s not found with %s: %s", resourceName, fieldName, fieldValue));
+    }
+
     public ResourceNotFoundException(String message) {
         super(message);
     }
